23d161426d9a8e7939e48fe3b2c455131f1a4702
Hywel862 <154019575+Hywel862@users.noreply.github.com>
2025-02-05 19:36:31 +0000

    Update services

--- src/main/java/toby/nichol/chess/com/api/service/ChessComService.java
+++ src/main/java/toby/nichol/chess/com/api/service/ChessComService.java
@@ -10,50 +10,70 @@ import org.springframework.web.client.RestTemplate;
 import toby.nichol.chess.com.api.model.GameStats;
 import toby.nichol.chess.com.api.model.Player;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import static toby.nichol.chess.com.api.service.helper.Helper.*;
 
 
 @Service
 @AllArgsConstructor
-public class ChessComService implements PlayerApiService {
+public class ChessComService {
 
     private final RestTemplate restTemplate;
 
-    public Player getPlayer(String player){
+    /**
+     * Retrieves the player stats.
+     * @param player String
+     * @return ResponseEntity<Player>
+     */
+    public ResponseEntity<Player> getPlayer(String player) {
         String url = getPlayerUrl(player);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json"); //<- accept the CSV file format
         HttpEntity<String> headersEntity = new HttpEntity<>(headers);
-        ResponseEntity<Player> response = restTemplate.exchange(
+        return restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 headersEntity,
                 Player.class
         );
-
-        if(response.getStatusCode().value() == 200){
-            return response.getBody();
-        }else{
-            throw new RuntimeException("Could not retrieve player.");
-        }
     }
 
-    public GameStats getPlayerGamesForDate(String player, String year, String month){
+    /**
+     * Retrieves the player stats for a given date.
+     * @param player String
+     * @param year String
+     * @param month String
+     * @return ResponseEntity<GameStats>
+     */
+    public ResponseEntity<GameStats> getPlayerGamesForDate(String player, String year, String month) {
         String url = getGamesUrl(player, year, month);
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json"); //<- accept the CSV file format
         HttpEntity<String> headersEntity = new HttpEntity<>(headers);
-        ResponseEntity<GameStats> response = restTemplate.exchange(
+        return restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 headersEntity,
                 GameStats.class
         );
+    }
 
-        if(response.getStatusCode().value() == 200){
-            return response.getBody();
-        }else{
-            throw new RuntimeException("Could not retrieve games.");
+    public ResponseEntity<String> getPlayerStatsForDate(String player, String year, String month) {
+        ResponseEntity<GameStats> gameStatsResponse = getPlayerGamesForDate(player, year, month);
+        if (gameStatsResponse.getBody() == null || gameStatsResponse.getBody().getGames() == null) {
+            return ResponseEntity.notFound().build();
         }
+        Map<String, Long> wls = gameStatsResponse.getBody().getGames().stream()
+                .filter(game -> game.getWhite().getUsername().equalsIgnoreCase("tobyChezz"))
+                .collect(Collectors.groupingBy(g -> g.getWhite().getResult().equalsIgnoreCase("win") ? "win" : "loss", Collectors.counting()));
+
+        int wins = wls.getOrDefault("win", 0L).intValue();
+        int losses = wls.getOrDefault("loss", 0L).intValue();
+
+        String ratio = calculateRatio(wins, losses);
+
+        return ResponseEntity.ok().body("In the month " + month + "/" + year + " you won " + wins + " and lost " + losses + " this is a ratio of " + ratio);
     }
 }
